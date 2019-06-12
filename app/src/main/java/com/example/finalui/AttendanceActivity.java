c4fad7fa5365311c4864c32a6f0053140c33eec9
HirakJyotiBorah <hborah862@gmail.com>
2019-06-12 14:41:18 +0530

    attendance for home activity completely done 3

--- app/src/main/java/com/example/finalui/AttendanceActivity.java
+++ app/src/main/java/com/example/finalui/AttendanceActivity.java
@@ -57,7 +57,6 @@ public class AttendanceActivity extends AppCompatActivity implements
         }
 
 
-        //Required
         datepic.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
